37dc2a39bcd87856fddea2aa0d9a3e0a5bd75e93
Leo Antelyes <lantelyes@gmail.com>
2025-06-21 10:32:38 -0400

    fx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Do NOT ignore TypeScript errors during build
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
